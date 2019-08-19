5615351413d29b52cc4e1db8fefd4f21e687415f
Akshay Khamkar <akshay.khamkar@perennialsys.com>
2019-08-19 10:25:29 +0530

    card features gif added

---
+++ index.js
@@ -0,0 +1,14 @@
+// Card Feature Collapse
+const sectionCard = document.getElementsByClassName("section-card-collapsible");
+
+for (let i = 0; i < sectionCard.length; i++) {
+  sectionCard[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    const sectionCardContent = this.nextElementSibling;
+    if (sectionCardContent.style.maxHeight){
+      sectionCardContent.style.maxHeight = null;
+    } else {
+      sectionCardContent.style.maxHeight = sectionCardContent.scrollHeight + "px";
+    }
+  });
+}
