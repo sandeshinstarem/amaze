a464103676c75e26805344a882263778d8f898b4
Sandesh <sandesh.uttarwar@perennialsys.com>
2019-08-19 11:15:41 +0530

    mobile is done

--- index.js
+++ index.js
@@ -2,13 +2,22 @@
 const sectionCard = document.getElementsByClassName("section-card-collapsible");
 
 for (let i = 0; i < sectionCard.length; i++) {
-  sectionCard[i].addEventListener("click", function() {
+  sectionCard[i].addEventListener("click", function () {
     this.classList.toggle("active");
     const sectionCardContent = this.nextElementSibling;
-    if (sectionCardContent.style.maxHeight){
+    if (sectionCardContent.style.maxHeight) {
       sectionCardContent.style.maxHeight = null;
     } else {
       sectionCardContent.style.maxHeight = sectionCardContent.scrollHeight + "px";
     }
   });
+}
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
